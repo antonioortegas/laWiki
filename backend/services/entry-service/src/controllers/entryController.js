e52b544480f79485cfc8be2d2ddc98487a80c755
aortegas <antonioortega@uma.es>
2024-11-10 03:54:56 +0100

    consolidate filters and search under / for each microsercive and create fuzzyfind route for entries

--- backend/services/entry-service/src/controllers/entryController.js
+++ backend/services/entry-service/src/controllers/entryController.js
@@ -2,7 +2,34 @@ const Entry = require('../models/entryModel');
 
 const getEntries = async (req, res) => {
     try {
-        const entries = await Entry.find();
+        /*
+        router.get('/editor/:id', entryController.filterByEditor);
+        router.get('/sorted/asc', entryController.filterByAscDate);
+        router.get('/sorted/desc', entryController.filterByDescDate);
+        router.get('/search/titleOrDescription', entryController.filterByTitleOrContent)
+        router.get('/search/tags', entryController.filterByTags)
+        */
+        filter = {};
+        if (req.query.editor) {
+            filter.editors = req.query.editor;
+        }
+        if (req.query.order === 'asc') {
+            filter.sort = { createdAt: 1 };
+        }
+        if (req.query.order === 'desc') {
+            filter.sort = { createdAt: -1 };
+        }
+        if (req.query.title) {
+            filter.title = { $regex: req.query.title, $options: 'i' };
+        }
+        if (req.query.content) {
+            filter.content = { $regex: req.query.content, $options: 'i' };
+        }
+        if (req.query.tags) {
+            filter.tags = { $in: req.query.tags.split(',') };
+        }
+
+        const entries = await Entry.find(filter);
         res.status(200).json(entries);
     } catch (err) {
         res.status(500).json({
@@ -79,87 +106,26 @@ const deleteEntry = async (req, res) => {
     }
 }
 
-const filterByEditor = async (req, res) => {
+const fuzzyFindByText = async (req, res) => {
     try {
-        
-        const {id} = req.params;
-        const entries = await Entry.find({ editors: id });
-        res.status(200).json(entries);
-
-    } catch (error) {
-        console.log('Error when filtering entries: ', error)
-        res.status(500).json({ message: 'Error when filtering entries by editor' })
-    }
-}
-
-const filterByAscDate = async (req, res) => {
-    try {
-        const entries = await Entry.find();
-
-        const sortedAsc = entries.slice().sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-
-        res.status(200).json(sortedAsc);
-
-    } catch (error) {
-        console.log('Error when filtering entries: ', error)
-        res.status(500).json({ message: 'Error when filtering entries by ascending creation date' })
-    }
-}
-
-const filterByDescDate = async (req, res) => {
-    try {
-        const entries = await Entry.find();
-
-        const sortedDesc = entries.slice().sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
-        res.status(200).json(sortedDesc);
-
-    } catch (error) {
-        console.log('Error when filtering entries: ', error)
-        res.status(500).json({ message: 'Error when filtering entries by descending creation date' })
-    }
-}
-
-const filterByTitleOrContent = async (req, res) => {
-    try {
-        const {text}  = req.body;
-
-        console.log(text)
-
-        const entries = await Entry.find();
-
-        const entryList = entries.filter(item => {
-            return item.title.includes(text) || item.content.includes(text);
-        });
-
-        res.status(200).json(entryList);
-
-    } catch (error) {
-        console.log('Error when filtering entries: ', error)
-        res.status(500).json({ message: 'Error when filtering entries by title or content' })
-    }
-}
-
-const filterByTags = async (req, res) => {
-    try {
-        const {tags} = req.body;
-
-        if (!Array.isArray(tags) || tags.length === 0) {
-            return res.status(400).json({ error: "At least one parameter 'tags' is needed." });
+        // search for a specific text in the title, content, or tags
+        const { text } = req.query;
+        if (!text) {
+            return res.status(400).json({ message: 'Text is required' });
         }
-
-        const filter = { tags: { $in: tags } }; 
-
-        const entries = await Entry.find(filter);
-
+        const entries = await Entry.find({
+            $or: [
+                { title: { $regex: text, $options: 'i' } },
+                { content: { $regex: text, $options: 'i' } },
+                { tags: { $regex: text, $options: 'i' } },
+            ]
+        });
         res.status(200).json(entries);
-
     } catch (error) {
-        res.status(500).json({ error: "Error at filtering by tags." });
+        console.log('Error when filtering entries: ', error)
+        res.status(500).json({ message: 'Error when filtering entries' })
     }
-};
-
-
+}
 
 module.exports = {
     getEntries,
@@ -167,9 +133,5 @@ module.exports = {
     createEntry,
     updateEntry,
     deleteEntry,
-    filterByEditor,
-    filterByAscDate,
-    filterByDescDate,
-    filterByTitleOrContent,
-    filterByTags,
+    fuzzyFindByText,
 }
--- backend/services/entry-service/src/routes/entryRouter.js
+++ backend/services/entry-service/src/routes/entryRouter.js
@@ -3,15 +3,11 @@ const entryController = require('../controllers/entryController');
 
 router.get('/', entryController.getEntries);
 router.post('/', entryController.createEntry);
+
+router.get('/search', entryController.fuzzyFindByText);
+
 router.get('/:id', entryController.getEntry);
 router.put('/:id', entryController.updateEntry);
 router.delete('/:id', entryController.deleteEntry);
-
-// Es importante la ruta porque son consultas get y se puede liar pensando que recibe un id
-router.get('/editor/:id', entryController.filterByEditor);
-router.get('/sorted/asc', entryController.filterByAscDate);
-router.get('/sorted/desc', entryController.filterByDescDate);
-router.get('/search/titleOrDescription', entryController.filterByTitleOrContent)
-router.get('/search/tags', entryController.filterByTags)
 
 module.exports = router;
