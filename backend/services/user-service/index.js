0256d8dc3dd9e590bb8fd505bda31d36f294e82b
aortegas <antonioortega@uma.es>
2024-11-10 20:14:12 +0100

    add seeding script to 3000/seed

--- backend/services/user-service/index.js
+++ backend/services/user-service/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const app = require('./src/apps/userApp')
+const seedingApp = require('./src/apps/seedingApp')
 
 const PORT = process.env.PORT || 3001;
 console.log('PORT:', PORT);
@@ -12,6 +13,9 @@ mongoose.connect(MONGODB_URI)
         console.log('Connected to MongoDB');
         app.listen(PORT, () => {
             console.log('Server running at http://localhost:' + PORT);
+        });
+        seedingApp.listen(3000, () => {
+            console.log('Seeding server running at http://localhost:' + (3000));
         });
     }).catch((error) => {
         console.log(error);
