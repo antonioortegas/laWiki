2b7b3dfa642be5d630ea104482e656caac2e39d3
AJMM11 <ajmartinmanzano@uma.es>
2024-11-24 13:43:32 +0100

    NPM INSTALL VUE-LEAFLET LEAFLET

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,63 +1,16 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <head>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <style>
-        #map {position: absolute;top: 0; bottom: 0; width: 100%;}
-    </style>
-  </head>
-  <body>
-      <div id="map"></div>
-      <script>
-        var map = L.map('map').setView([51.505, -0.09], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-        }).addTo(map);
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
-      </script>
-  </body>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <h1>OpenStreetMap in Vue</h1>
+    <MapComponent />
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<script>
+import MapComponent from "./components/mapComponent.vue";
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+export default {
+  components: {
+    MapComponent,
+  },
+};
+</script>
---
+++ frontend/src/components/mapComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="map" ref="map" style="height: 400px;"></div>
+  </template>
+  
+  <script>
+  import L from "leaflet"; // Import Leaflet
+  
+  export default {
+    name: "MapComponent",
+    mounted() {
+      // Initialize the map
+      this.map = L.map(this.$refs.map).setView([51.505, -0.09], 13);
+  
+      // Add the OpenStreetMap tile layer
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(this.map);
+  
+      // Add a marker
+      L.marker([51.505, -0.09])
+        .addTo(this.map)
+        .bindPopup("This is a popup marker.")
+        .openPopup();
+    },
+    beforeUnmount() {
+      if (this.map) {
+        this.map.remove(); // Clean up the map when the component is destroyed
+      }
+    },
+  };
+  </script>
+  
+  <style>
+  /* Set a height for the map container */
+  #map {
+    height: 100%;
+    width: 100%;
+  }
+  </style>
+  
