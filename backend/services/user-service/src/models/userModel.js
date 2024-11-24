5625714f0d1c9d128d13c711c5451632af5b4fb6
NachoK66 <nachok662000@gmail.com>
2024-11-24 17:03:44 +0100

    added param for email notifications on user

--- backend/services/user-service/src/models/userModel.js
+++ backend/services/user-service/src/models/userModel.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    getNotificationsByEmail: {
+        type: Boolean,
+        default: false
+    },
     role: {
         type: String,
         required: true,
