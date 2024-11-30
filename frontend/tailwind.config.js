c2ba7782368621b9235d5bff33b6096aacf7310f
aortegas <antonioortega@uma.es>
2024-11-30 18:05:57 +0100

    actually fix color imports in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -11,12 +10,6 @@ export default {
         primary: '#18c95a',
         secondary: '#a9a7a9',
         accent: '#00bfae',
-        lightBlue: undefined,
-        warmGray: undefined,
-        trueGray: undefined,
-        coolGray: undefined,
-        blueGray: undefined,
-        ...colors, // Include Tailwind's default colors
       },
       fontFamily: {
         heading: ['Ubuntu', 'sans-serif'],
