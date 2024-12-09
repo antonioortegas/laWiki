a3a11c633c956cf53cc3b67e45c8f6d39ea2f0bf
Ignacio Morillas Rosell <nachok662000@gmail.com>
2024-12-10 00:49:17 +0100

    Added restriction to not vote yourself

--- frontend/src/components/StarRating.vue
+++ frontend/src/components/StarRating.vue
@@ -77,21 +77,27 @@ export default {
     },
     // Sends the selected rating to the server, if the user is a writer
     async setRating(star) {
+      const loggedUser = await axios.get(`/api/users/${this.loggedUserId}`);
+      
       if(loggedUser.data.role === 'writer' || loggedUser.data.role === 'admin') {
-        try {
-          const loggedUser = await axios.get(`/api/users/${this.loggedUserId}`);
-          await axios.post(`/api/users/${this.profileUserId}/addRating`, {
-            ratedBy: this.loggedUserEmail,
-            score: star,
-          });
-  
-          console.log("Sent rating:", star);
-          console.log("For user ID:", this.profileUserId);
-          console.log("By logged user ID:", this.loggedUserEmail);
-  
-          this.$emit("update:value", star); // Update the rating locally
-        } catch (error) {
-          console.error("Error submitting rating:", error.response?.data || error.message);
+        if(loggedUser.data._id === this.profileUserId) {
+          console.log("You can't rate yourself!");
+          return;
+        } else {
+          try {
+            await axios.post(`/api/users/${this.profileUserId}/addRating`, {
+              ratedBy: this.loggedUserEmail,
+              score: star,
+            });
+    
+            console.log("Sent rating:", star);
+            console.log("For user ID:", this.profileUserId);
+            console.log("By logged user ID:", this.loggedUserEmail);
+    
+            this.$emit("update:value", star); // Update the rating locally
+          } catch (error) {
+            console.error("Error submitting rating:", error.response?.data || error.message);
+          }
         }
       }
     }
