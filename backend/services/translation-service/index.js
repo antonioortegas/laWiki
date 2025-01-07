ea7bbbb82c3ed234e0d1278371e7f8348a48bfff
aortegas <antonioortega@uma.es>
2025-01-07 16:13:50 +0100

    install cors on traslation service and make the port dynamic by env variable ffs

--- backend/services/translation-service/index.js
+++ backend/services/translation-service/index.js
@@ -1,5 +1,6 @@
 const appTranslation = require("./src/apps/appTranslation");
+const PORT = process.env.PORT || 4001;
 
-appTranslation.listen(4001, () => 
-    console.log("The trasnlation microservice is running on port 4001")
+appTranslation.listen(PORT, () => 
+    console.log("The trasnlation microservice is running on port " + PORT)
 );
--- backend/services/translation-service/src/apps/appTranslation.js
+++ backend/services/translation-service/src/apps/appTranslation.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const routerTranslation = require('../routes/routesTranslation')
+const cors = require('cors')
 
 const app = express()
 
 app.use(morgan('dev2'))
 app.use(express.json()) 
+app.use(cors())
 
 app.use('/translation', routerTranslation)
 
