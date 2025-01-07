f233e0f3f4271f78fff7a0599f1b6bf64c6137b1
aortegas <antonioortega@uma.es>
2025-01-07 03:13:24 +0100

    try to fix frontend deploy failure

--- backend/services/wiki-service/src/apps/wikiApp.js
+++ backend/services/wiki-service/src/apps/wikiApp.js
@@ -2,9 +2,14 @@ const express = require('express')
 const wikiRoutes = require('../routes/wikiRouter')
 const cors = require('cors')
 
+//allow cross-origin requests from any domain
+var corsOptions = {
+  origin: '*',
+}
+
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/wikis', wikiRoutes)
 
